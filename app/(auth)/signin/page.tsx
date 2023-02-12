339fa45aedeccb145e799ecc3679aff06c9805a8
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-02-12 16:59:53 +0300

    Created Register and SignIn Pages

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,3 +1,9 @@
-export default function SignIn(){
-    return <div>SignIn</div>
+import AuthForm from "@/components/AuthForm";
+
+export default function SignIn() {
+  return (
+    <div>
+      <AuthForm mode="signin" />
+    </div>
+  );
 }
