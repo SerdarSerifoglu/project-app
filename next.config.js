8af57302018ecab3c38abc193b6160dd23c53aeb
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-02-24 12:02:45 +0300

    eslint ignoreDuringBuilds: true

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     appDir: true,
     serverComponentsExternalPackages: ["bcrypt"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
